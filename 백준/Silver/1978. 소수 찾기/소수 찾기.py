1a0ecbab623cf38b7f734c6bede8f9c17cea3a66
Kminwo-o <108923472+Kminwo-o@users.noreply.github.com>
2023-01-21 17:00:08 +0900

    [Silver V] Title: 소수 찾기, Time: 40 ms, Memory: 30616 KB -BaekjoonHub

---
+++ 백준/Silver/1978. 소수 찾기/소수 찾기.py
@@ -0,0 +1,17 @@
+N = int(input())
+
+a = list(map(int,input().split()))
+cnt = 0
+
+for i in a:
+    if i <= 1:
+        continue
+        
+    else:
+        for j in range(2,i):
+            if i % j == 0:
+                break
+        else:
+            cnt += 1
+        
+print(cnt)
