224f9fc54f628a76fdda16c5a5908c3aac7a148b
Kminwo-o <108923472+Kminwo-o@users.noreply.github.com>
2023-01-22 02:05:03 +0900

    [Bronze III] Title: ACM 호텔, Time: 36 ms, Memory: 30616 KB -BaekjoonHub

--- 백준/Bronze/10250. ACM 호텔/ACM 호텔.py
+++ 백준/Bronze/10250. ACM 호텔/ACM 호텔.py
@@ -4,17 +4,8 @@ input = sys.stdin.readline
 T = int(input())
 
 for _ in range(T):
-    H,W,N = map(int,input().split())
-    checkin_lst = []
-    cnt = 0
-
-    for j in range(1, W+1):
-        if cnt == N:
-            break
-
-        for k in range(1, H+1):
-            if cnt == N:
-                break
-            checkin_lst.append((k*100 + j))
-            cnt += 1
-    print(checkin_lst[-1])
+    H, W, N = map(int, input().split())
+    if N%H == 0:
+        print(H*100 + N//H)
+    else:
+        print(N%H*100 + N//H + 1)
