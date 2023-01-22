5735f467556521f9c9b8782a7668bdf59d378dfc
Kminwo-o <108923472+Kminwo-o@users.noreply.github.com>
2023-01-22 22:26:31 +0900

    [Silver IV] Title: 스택, Time: 48 ms, Memory: 30616 KB -BaekjoonHub

--- 백준/Silver/10828. 스택/스택.py
+++ 백준/Silver/10828. 스택/스택.py
@@ -7,25 +7,25 @@ stack = []
 for i in range(N):
     order = input().split()
     if 'push' in order:
-        stack.append(int(order[1]))
+        stack.append(order[1])
     
-    if 'top' in order  and 0 < len(stack):
-        print(stack[-1])
-
-    elif 'top' in order and 0 == len(stack):
-        print(-1)
+    if 'top' in order:
+        if 0 < len(stack):
+            print(stack[-1])
+        else: 
+            print(-1)
 
     if 'size' in order:
         print(len(stack))
     
-    if 'empty' in order and 0 == len(stack):
-        print(1)
+    if 'empty' in order:
+        if 0 == len(stack):
+            print(1)
+        else:
+            print(0)
     
-    elif 'empty' in order:
-        print(0)
-    
-    if 'pop' in order and 0 == len(stack):
-        print(-1)
-    
-    elif 'pop' in order:
-        print(stack.pop(-1))
+    if 'pop' in order:
+        if 0 == len(stack):
+            print(-1)
+        else:
+            print(stack.pop(-1))
