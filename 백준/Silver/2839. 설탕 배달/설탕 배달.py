5d58eb5d35d30fbcd4675d9469d152cc361b20ee
Kminwo-o <108923472+Kminwo-o@users.noreply.github.com>
2023-02-01 23:10:51 +0900

    [Silver IV] Title: 설탕 배달, Time: 44 ms, Memory: 31256 KB -BaekjoonHub

--- 백준/Silver/2839. 설탕 배달/설탕 배달.py
+++ 백준/Silver/2839. 설탕 배달/설탕 배달.py
@@ -1,24 +1,16 @@
-import sys
-input = sys.stdin.readline
-
 N = int(input())
-sugar = [-1 for _ in range(5001)]
+cnt = 0
 
-sugar[3] = 1
-sugar[5] = 1
+while True:
+    if N < 0:
+        cnt = -1
+        break
 
-if N <= 5:
-    print(sugar[N])
+    if N % 5 == 0:
+        cnt += (N // 5)
+        break
 
-else:
-    for i in range(6, N+1):
-        if i % 5 == 0:
-            sugar[i] = sugar[i-5] + 1
-        
-        elif i % 3 == 0:
-            sugar[i] = sugar[i-3] + 1
+    N -= 3
+    cnt += 1
 
-        elif sugar[i-3] > 0 and sugar[i-5] > 0:
-            sugar[i] = min(sugar[i-3], sugar[i-5]) + 1
-    
-    print(sugar[N])
+print(cnt)
