dc9e7dbb6ed2ee4f58a571a5924394b0d61c0936
Kminwo-o <108923472+Kminwo-o@users.noreply.github.com>
2023-03-01 23:29:25 +0900

    [Gold V] Title: 토마토, Time: 1156 ms, Memory: 98592 KB -BaekjoonHub

--- 백준/Gold/7576. 토마토/토마토.py
+++ 백준/Gold/7576. 토마토/토마토.py
@@ -6,46 +6,39 @@ from collections import deque
 d = [(-1, 0), (1, 0), (0, -1), (0, 1)]
 
 def bfs(lst):
-    visited = [[0] * N for _ in range(M)]
-    queue = deque([])
 
-    for i in tomato_lst:
-        queue.append(i)
-        visited[i[0]][i[1]] = 1
-
-    while queue:
-        a, b = queue.popleft()
+    while tomato_lst:
+        a, b = tomato_lst.popleft()
 
         for dr, dc in d:
             nr = a + dr
             nc = b + dc
 
-            if 0 <= nr < M and 0 <= nc < N and tomato_bat[nr][nc] == '0' and not visited[nr][nc]:
-                visited[nr][nc] = visited[a][b] + 1
-                tomato_bat[nr][nc] = '1'
-                queue.append((nr, nc))
+            if 0 <= nr < M and 0 <= nc < N and tomato_bat[nr][nc] == 0:
+                tomato_bat[nr][nc] = tomato_bat[a][b] + 1
+                tomato_lst.append((nr, nc))
 
     max_ = 0
 
     for i in range(M):
         for j in range(N):
-            if tomato_bat[i][j] == '0':
+            if tomato_bat[i][j] == 0:
                 max_ = 0
                 return max_
 
-            if max_ < visited[i][j]:
-                max_ = visited[i][j]
+            if max_ < tomato_bat[i][j]:
+                max_ = tomato_bat[i][j]
 
     return max_
 
 N, M = map(int, input().split())
 
-tomato_bat = [input().split() for _ in range(M)]
-tomato_lst = []
+tomato_bat = [list(map(int, input().split())) for _ in range(M)]
+tomato_lst = deque()
 
 for i in range(M):
     for j in range(N):
-        if tomato_bat[i][j] == '1':
+        if tomato_bat[i][j] == 1:
             tomato_lst.append((i, j))
 
 print(bfs(tomato_lst)-1)
