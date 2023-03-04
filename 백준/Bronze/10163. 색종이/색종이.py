56a94e0b9b5af447db97a6992da9a3681a04ab14
Kminwo-o <108923472+Kminwo-o@users.noreply.github.com>
2023-03-04 17:26:40 +0900

    [Bronze I] Title: 색종이, Time: 316 ms, Memory: 38436 KB -BaekjoonHub

--- 백준/Bronze/10163. 색종이/색종이.py
+++ 백준/Bronze/10163. 색종이/색종이.py
@@ -7,25 +7,19 @@ N = int(input())
 arr = [[0] * 1001 for _ in range(1001)]
 lst = []
 location = []
-for _ in range(N):
+for l in range(1,N+1):
     x, y, w, h = map(int,input().split())
-    
+
     for i in range(y, h+y):
         for j in range(x, w+x):
-            arr[i][j] = 1
+            arr[i][j] = l
 
-    location.append((x, y, w, h))
+ans = [0] * (N+1)
 
-ans = []
+for i in range(1001):
+    for j in range(1001):
+        if arr[i][j]:
+            ans[arr[i][j]] += 1
 
-for i in range(len(location)-1,-1,-1):
-    cnt = 0
-    for j in range(location[i][1],location[i][3]+location[i][1]):
-        for k in range(location[i][0], location[i][2]+location[i][0]):
-            if arr[j][k] == 1:
-                cnt += 1
-                arr[j][k] = 0
-    ans.append(cnt)
-
-for i in range(len(ans)-1, -1, -1):
+for i in range(1,N+1):
     print(ans[i])
