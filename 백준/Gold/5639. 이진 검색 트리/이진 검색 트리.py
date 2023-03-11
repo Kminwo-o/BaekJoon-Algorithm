792c11743ecea8e030876ca2a745f6035522b867
Kminwo-o <108923472+Kminwo-o@users.noreply.github.com>
2023-03-11 17:14:32 +0900

    [Gold V] Title: 이진 검색 트리, Time: 240 ms, Memory: 134288 KB -BaekjoonHub

--- 백준/Gold/5639. 이진 검색 트리/이진 검색 트리.py
+++ 백준/Gold/5639. 이진 검색 트리/이진 검색 트리.py
@@ -1,10 +1,10 @@
 import sys
 input = sys.stdin.readline
-sys.setrecursionlimit(10**6)
+sys.setrecursionlimit(10**4)
 
 def preorder(start, end):
-    if start > end:
-        return
+    global node_lst
+    if start > end: return
     
     # 오른쪽이 없을 경우 오른쪽 순회를 바로 끝내기 위해 +1
     mid = end + 1
@@ -14,7 +14,6 @@ def preorder(start, end):
         if node_lst[start] < node_lst[i]:
             mid = i
             break
-    
     # 왼쪽
     preorder(start+1, mid-1)
     # 오른쪽
@@ -26,9 +25,7 @@ node_lst = []
 
 # 입력 안되면 걍 그만
 while True:
-    try:
-        node_lst.append(int(input().strip()))
-    except:
-        break
+    try: node_lst.append(int(input().strip()))
+    except: break
 
 preorder(0, len(node_lst)-1)
