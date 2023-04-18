10f5492d441a6d58b7793ba1778fd9bedaa8c247
Kminwo-o <108923472+Kminwo-o@users.noreply.github.com>
2023-04-18 16:12:02 +0900

    [Gold IV] Title: 가장 긴 증가하는 부분 수열 4, Time: 48 ms, Memory: 31256 KB -BaekjoonHub

--- 백준/Gold/14002. 가장 긴 증가하는 부분 수열 4/가장 긴 증가하는 부분 수열 4.py
+++ 백준/Gold/14002. 가장 긴 증가하는 부분 수열 4/가장 긴 증가하는 부분 수열 4.py
@@ -1,24 +1,36 @@
 import sys
 input = sys.stdin.readline
 
-N = int(input())
-lst = list(map(int,input().split()))
-dp = [1 for _ in range(N)]
+def binary_search(target):
+    s, e = 0, len(lst)-1
+    while s<=e:
+        mid = (s+e)//2
+        if target <= lst[mid]:
+            e = mid - 1
+        elif target > lst[mid]:
+            s = mid + 1
+    return s
+
+INF = int(1e9)
+n = int(input())
+arr = list(map(int, input().split()))
+lst = [-INF]
+dp = [1]*(n+1)
+
+for i in range(n):
+    if lst[-1] < arr[i]:
+        lst.append(arr[i])
+        dp[i] = len(lst)-1
+    else:
+        dp[i] = binary_search(arr[i])
+        lst[dp[i]] = arr[i]
+
+N = max(dp)     # 최대 수열길이
+print(N)
 ans = []
+for j in range(n-1, -1, -1):
+    if dp[j] == N:
+        ans.append(arr[j])
+        N -= 1
 
-for i in range(N):
-    for j in range(i+1):
-        if lst[i] > lst[j]:
-            dp[i] = max(dp[i], dp[j]+1)
-
-best_cnt = max(dp)
-print(best_cnt)
-
-for i in range(N-1,-1,-1):
-    if dp[i] == best_cnt:
-        ans.append(lst[i])
-        best_cnt -= 1
-
-ans.sort()
-
-print(*ans)
+print(*ans[::-1])
