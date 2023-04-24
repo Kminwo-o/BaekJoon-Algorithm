913a94eb296d284c8a1cce5efb7064091a7f4040
Kminwo-o <108923472+Kminwo-o@users.noreply.github.com>
2023-04-24 21:41:26 +0900

    [Silver II] Title: 잃어버린 괄호, Time: 124 ms, Memory: 14164 KB -BaekjoonHub

--- 백준/Silver/1541. 잃어버린 괄호/잃어버린 괄호.java
+++ 백준/Silver/1541. 잃어버린 괄호/잃어버린 괄호.java
@@ -1,25 +1,28 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.StringTokenizer;
 
 public class Main {
     public static void main(String[] args) throws IOException {
     	BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-    	
-    	String[] num = br.readLine().split("-");
+    	StringTokenizer st = new StringTokenizer(br.readLine(), "-");
     	int sum = Integer.MAX_VALUE;
-    	for (int i = 0; i < num.length; i++) {
-			int tmp = 0;
-			String[] tmp_num = num[i].split("\\+");
-			for (int j = 0; j < tmp_num.length; j++) {
-				tmp += Integer.parseInt(tmp_num[j]);
-			}
-			if (i == 0) {
-				sum = tmp;
-			} else {
-				sum -= tmp;
-			}
-		}
-    	System.out.println(sum);
+    	
+    	while (st.hasMoreTokens()) {
+    		int tmp = 0;
+    		
+    		StringTokenizer add = new StringTokenizer(st.nextToken(), "+");
+    		
+    		while (add.hasMoreTokens()) {
+    			tmp += Integer.parseInt(add.nextToken());
+    		}
+    		if (sum == Integer.MAX_VALUE) {
+    			sum = tmp;
+    		} else {
+    			sum -= tmp;
+    		}
+    	}
+    	System.out.println(sum);    	
     }
 }
