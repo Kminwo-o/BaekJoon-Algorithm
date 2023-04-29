633bb843fd2e9895251f0ff5bfe758ac4a97ccdb
Kminwo-o <108923472+Kminwo-o@users.noreply.github.com>
2023-04-30 00:36:17 +0900

    [Silver III] Title: 주유소, Time: 368 ms, Memory: 31152 KB -BaekjoonHub

---
+++ 백준/Silver/13305. 주유소/주유소.java
@@ -0,0 +1,41 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Iterator;
+import java.util.StringTokenizer;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+    	// 처음 받을 때 문자열 그대로 받기
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        
+        // 담은 토큰들 n, m에 넣어주기
+        int N = Integer.parseInt(br.readLine()); // 도시의 수
+        int[] road_lst = new int[N-1];
+        int[] cost = new int[N];
+        
+        StringTokenizer st = new StringTokenizer(br.readLine(), " ");
+        for (int i = 0; i < N-1; i++) {
+			road_lst[i] = Integer.parseInt(st.nextToken());
+		}
+        
+        st = new StringTokenizer(br.readLine());
+        for (int i = 0; i < N; i++) {
+			cost[i] = Integer.parseInt(st.nextToken());
+		}
+        
+        int sum = 0;
+        int low_cost = cost[0];
+        
+        for (int i = 0; i < N-1; i++) {
+			if (cost[i] < low_cost) {
+				low_cost = cost[i];
+			}
+			
+			sum += (low_cost * road_lst[i]);
+
+		}
+        
+        System.out.println(sum);
+    }
+}
