bc85fbe6e71b92c9decbacd663044b240a6bb769
Kminwo-o <108923472+Kminwo-o@users.noreply.github.com>
2023-07-16 01:21:49 +0900

    [Gold V] Title: 장작 넣기, Time: 644 ms, Memory: 16456 KB -BaekjoonHub

--- 백준/Gold/24884. 장작 넣기/장작 넣기.java
+++ 백준/Gold/24884. 장작 넣기/장작 넣기.java
@@ -1,106 +1,76 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 import java.util.*;
-import java.io.*;
 
 public class Main {
-    static int N, W, T, K; 
-    static int map[];      
-    static int wood[];    
-    static int process[];  
-    static int temp[];    
-    static int cur = -1;   
-    static int ans = 0;   
-    public static void main(String[] args) throws Exception {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-
-        StringTokenizer st = new StringTokenizer(br.readLine());
-
-        N = Integer.parseInt(st.nextToken()); 
-        W = Integer.parseInt(st.nextToken())+1;
-        T = Integer.parseInt(st.nextToken());
-        K = Integer.parseInt(st.nextToken());
-
-        wood = new int[T + 1]; 
-        map = new int[N + 1];
-        process = new int[N + 1];
-        temp = new int[N + 1];
-
-        st = new StringTokenizer(br.readLine());
-
-        for (int i = 1; i <= N; i++) {
-            map[i] = Integer.parseInt(st.nextToken());
-        }
-
-        wood[0] = W;
-        site(1);
-        System.out.println(ans);
-    }
-    public static void down(int time) {
-        int size = 3;
+    static int N, W, T, K;
+    static int[] wood;
+    static int[] fire;
+    static int[] temp;
+    static int[] process;
+    static int new_wood;
+    static int ans = 0;
+    public static void fire_minus (int time) {
         for (int i = 1; i <= N; i++) {
             temp[i] = process[i];
         }
 
         for (int i = 1; i <= N; i++) {
-            size = 3;
-            if (cur == i) {
-                continue;
-            }
-
+            int down = 3;
+            if (new_wood == i) continue;
             if (i > 1 && i < N) {
-                if (process[i - 1] > 0) {
-                    size -= 1;
-                }
-                if (process[i + 1] > 0) {
-                    size -= 1;
-                }
+                if (process[i + 1] > 0) down--;
+                if (process[i - 1] > 0) down--;
             } else if (i == N) {
-                if (process[i - 1] > 0) {
-                    size -= 1;
-                }
-            } else if (i == 1) {
-                if (process[i + 1] > 0) {
-                    size -= 1;
-                }
+                if (process[i - 1] > 0) down--;
+            } else {
+                if (process[i + 1] > 0) down--;
             }
-            temp[i] -= size;
+            temp[i] -= down;
         }
 
-        cur = wood[time];
+        new_wood = wood[time];
 
         for (int i = 1; i <= N; i++) {
             process[i] = temp[i];
         }
     }
-    public static void site(int time) {
+
+    public static void dfs (int time) {
         if (time == T) {
             if (!check()) {
                 return;
             }
 
             for (int i = 1; i <= N; i++) {
-                process[i] = map[i];
+                process[i] = fire[i];
             }
-            
-            cur = -1; // 
+            new_wood = -1;
 
             for (int i = 1; i <= T; i++) {
-                down(i);
-            }
-            
-            if (min()) {
-                ans++;
+                fire_minus(i);
             }
 
+            int cnt = 0;
+            for (int i = 1; i <= N; i++) {
+                if (process[i] > 0) {
+                    cnt++;
+                }
+            }
+            if (cnt >= K) {
+                ans++;
+            }
             return;
         }
 
         for (int i = 1; i <= N; i++) {
             wood[time] = i;
-            site(time + 1);
+            dfs(time+1);
             wood[time] = 0;
         }
     }
-    
+
     public static boolean check() {
         for (int i = 1; i < T; i++) {
             if (Math.abs(wood[i] - wood[i - 1]) > 1) {
@@ -110,19 +80,29 @@ public class Main {
         return true;
     }
 
-    public static boolean min() {
-        int count = 0;
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        N = Integer.parseInt(st.nextToken());
+        W = Integer.parseInt(st.nextToken())+1;
+        T = Integer.parseInt(st.nextToken());
+        K = Integer.parseInt(st.nextToken());
+
+        fire = new int[N+1];
+        temp = new int[N+1];
+        process = new int[N+1];
+        wood = new int[T+1];
+
+
+        st = new StringTokenizer(br.readLine());
 
         for (int i = 1; i <= N; i++) {
-            if (process[i] > 0) {
-                count++;
-            }
-        }
-        
-        if (count < K) {
-            return false;
+            fire[i] = Integer.parseInt(st.nextToken());
         }
 
-        return true;
+        wood[0] = W;
+        dfs(1);
+        System.out.println(ans);
     }
 }
