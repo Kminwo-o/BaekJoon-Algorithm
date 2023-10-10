4e37c9a0a53ddc887c02b95a15e2308005f58eb7
Kminwo-o <108923472+Kminwo-o@users.noreply.github.com>
2023-10-10 14:51:20 +0900

    [Silver I] Title: 배열 돌리기 1, Time: 564 ms, Memory: 33340 KB -BaekjoonHub

--- 백준/Silver/16926. 배열 돌리기 1/배열 돌리기 1.java
+++ 백준/Silver/16926. 배열 돌리기 1/배열 돌리기 1.java
@@ -1,9 +1,6 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.Arrays;
-import java.util.HashMap;
-import java.util.Map;
 import java.util.StringTokenizer;
 
 class Main{
@@ -11,32 +8,6 @@ class Main{
     static int M;
     static int R;
     static int[][] arr;
-    static int[][] dir = {{1, 0}, {0, 1}, {-1, 0}, {0, -1}};
-
-    public static int[][] rotation(int half, int[][] tmp_arr) {
-        // 회전 시작점
-        for (int i = 0; i < half; i++) {
-            int x = i;
-            int y = i;
-
-            int direct = 0;
-            while (direct < 4) {
-                int nx = x + dir[direct][0];
-                int ny = y + dir[direct][1];
-
-                if ((nx >= 0 + i && nx < N - i) && (ny >= 0 + i && ny < M - i)) {
-                    tmp_arr[nx][ny] = arr[x][y];
-
-                    x = nx;
-                    y = ny;
-
-                    continue;
-                }
-                direct++;
-            }
-        }
-        return tmp_arr;
-    }
 
     public static void main(String[]args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
@@ -44,8 +15,6 @@ class Main{
         N = Integer.parseInt(st.nextToken());
         M = Integer.parseInt(st.nextToken());
         R = Integer.parseInt(st.nextToken());
-
-        Map <String, Integer> map = new HashMap<>();
 
         arr = new int[N][M];
 
@@ -59,10 +28,41 @@ class Main{
         int len = Math.min(N, M) / 2;
 
         for (int i = 0; i < R; i++) {
-            int[][] tmpArr = new int[N][M];
-            arr = rotation(len, tmpArr);
+            for (int j = 0; j < len; j++) {
+                int x = j;
+                int y = j;
+                int tmp = arr[j][j];
+
+                for (int k = j + 1; k < N - j; k++) {
+                    x = k;
+                    int prev_value = arr[x][y];
+                    arr[x][y] = tmp;
+                    tmp = prev_value;
+                }
+
+                for (int k = j + 1; k < M - j; k++) {
+                    y = k;
+                    int prev_value = arr[x][y];
+                    arr[x][y] = tmp;
+                    tmp = prev_value;
+                }
+
+                for (int k = j + 1; k < N - j; k++) {
+                    x = N - k - 1;
+                    int prev_value = arr[x][y];
+                    arr[x][y] = tmp;
+                    tmp = prev_value;
+                }
+
+                for (int k = j + 1; k < M - j; k++) {
+                    y = M - k - 1;
+                    int prev_value = arr[x][y];
+                    arr[x][y] = tmp;
+                    tmp = prev_value;
+                }
+            }
         }
-        
+
         StringBuilder sb = new StringBuilder();
         for(int i=0;i<N;i++) {
             for(int j=0;j<M;j++) {
