d11752493dfac58b78eb6884b2781d73fe552c8b
KwonOo <108923472+Kminwo-o@users.noreply.github.com>
2023-10-19 09:58:34 +0900

    [Silver I] Title: 나이트의 이동, Time: 256 ms, Memory: 63072 KB -BaekjoonHub

--- 백준/Silver/7562. 나이트의 이동/나이트의 이동.java
+++ 백준/Silver/7562. 나이트의 이동/나이트의 이동.java
@@ -1,51 +1,74 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.*;
+import java.util.LinkedList;
+import java.util.Queue;
+import java.util.StringTokenizer;
 
-public class Main {
-	static int N, T;
-	static int[][] dir = {{-2, -1}, {-1, -2}, {-2, 1}, {-1, 2}, {1, -2}, {2, -1}, {2, 1}, {1, 2}};
-	static StringTokenizer st;
-	static int[][] visited;
-	static int[][] location;
-	public static void bfs(int x, int y) {
-		Queue<int[]> q = new LinkedList<>();
-		q.add(new int[] {x, y});
-		
-		while (!q.isEmpty()) {
-			int now[] = q.poll();
-			if (location[1][0] == now[0] && location[1][1] == now[1]) {
-				break;
-			}
-			for (int[] is : dir) {
-				int nx = now[0] + is[0];
-				int ny = now[1] + is[1];
-				
-				if ((nx >= 0 && nx < N) && (ny >= 0 && ny < N) && visited[nx][ny] == 0)  {
-					visited[nx][ny] = visited[now[0]][now[1]] + 1;
-					q.add(new int[] {nx, ny});
-				}
-			}
-		}
-	}
-	
-	public static void main(String[] args) throws IOException{
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		T = Integer.parseInt(br.readLine());
-		
-		for (int i = 0; i < T; i++) {
-			N = Integer.parseInt(br.readLine());
-			visited = new int[N][N];
-			location = new int[2][2];
-			for (int j = 0; j < 2; j++) {
-				st = new StringTokenizer(br.readLine());
-				location[j][0] = Integer.parseInt(st.nextToken());
-				location[j][1] = Integer.parseInt(st.nextToken());
-			}
-			bfs(location[0][0], location[0][1]);
+public class Main{
 
-			System.out.println(visited[location[1][0]][location[1][1]]);
-		}
-	}
+    static int T, N;
+    static boolean[][] visited;
+    static int[][] dir = {{-2, -1}, {-1, -2}, {2, -1}, {1, -2}, {2, 1}, {1, 2}, {-1, 2}, {-2, 1}};
+
+    static class Node {
+        int x;
+        int y;
+        int cnt;
+
+        public Node (int x, int y, int cnt) {
+            this.x = x;
+            this.y = y;
+            this.cnt = cnt;
+        }
+    }
+
+    public static int bfs(int sx, int sy, int ex, int ey) {
+        Queue<Node> queue = new LinkedList<>();
+        queue.add(new Node(sx, sy, 0));
+        visited[sx][sy] = true;
+
+        while (!queue.isEmpty()) {
+            Node node = queue.poll();
+
+            if (node.x == ex && node.y == ey) {
+                return node.cnt;
+            }
+
+            for (int i = 0; i < 8; i++) {
+                int nx = node.x + dir[i][0];
+                int ny = node.y + dir[i][1];
+
+                if (nx >= 0 && nx < N && ny >= 0 && ny < N && !visited[nx][ny]) {
+                    queue.add(new Node(nx, ny, node.cnt + 1));
+                    visited[nx][ny] = true;
+                }
+            }
+        }
+        return 0;
+    }
+
+    public static void main(String[] args) throws IOException{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int sx, sy, ex, ey;
+        int answer;
+        StringTokenizer st;
+
+        T = Integer.parseInt(br.readLine());
+
+        for (int i = 0; i < T; i++) {
+            N = Integer.parseInt(br.readLine());
+            visited = new boolean[N][N];
+            st = new StringTokenizer(br.readLine());
+            sx = Integer.parseInt(st.nextToken());
+            sy = Integer.parseInt(st.nextToken());
+
+            st = new StringTokenizer(br.readLine());
+            ex = Integer.parseInt(st.nextToken());
+            ey = Integer.parseInt(st.nextToken());
+
+            answer = bfs(sx, sy, ex, ey);
+            System.out.println(answer);
+        }
+    }
 }
