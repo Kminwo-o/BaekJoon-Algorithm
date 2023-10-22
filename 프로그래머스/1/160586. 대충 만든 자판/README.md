869146a640c13bd9edfe7276d817e85cce60ddd5
KwonOo <108923472+Kminwo-o@users.noreply.github.com>
2023-10-22 14:04:45 +0900

    [level 1] Title: 대충 만든 자판, Time: 22.14 ms, Memory: 93.9 MB -BaekjoonHub

--- 프로그래머스/1/160586. 대충 만든 자판/README.md
+++ 프로그래머스/1/160586. 대충 만든 자판/README.md
@@ -1,10 +1,10 @@
 # [level 1] 대충 만든 자판 - 160586 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/160586#) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/160586#qna) 
 
 ### 성능 요약
 
-메모리: 79.8 MB, 시간: 9.86 ms
+메모리: 93.9 MB, 시간: 22.14 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2023년 10월 6일 23:53:6
+2023년 10월 0일 14:4:41
 
 ### 문제 설명
 
--- 프로그래머스/1/160586. 대충 만든 자판/대충 만든 자판.java
+++ 프로그래머스/1/160586. 대충 만든 자판/대충 만든 자판.java
@@ -1,30 +1,53 @@
-import java.util.Arrays;
+ import java.util.Arrays;
 
 class Solution {
     public int[] solution(String[] keymap, String[] targets) {
-        int[] answer = new int[targets.length];        
+        int[] answer = new int[targets.length];
         
-        for(int i=0; i<targets.length; i++) {
-        	for(int j=0; j<targets[i].length(); j++) {
-        		int cnt = 101;
-        		for(int k = 0; k<keymap.length; k++) {
-        			int loc = keymap[k].indexOf(targets[i].charAt(j));
-        			if(loc != -1) {
-        				if(cnt > loc) {
-        					cnt = loc;
-        				}
-        			}
-        		}
-        		if(cnt == 101) {
-        			answer[i] = -1;
-        			break;
-        		}
-        		else {
-        			answer[i] += cnt +1;
-        		}        		        		
-        	}
+        Arrays.fill(answer, -1);
+        
+        // 목표 단어의 개수
+        for (int i=0; i < targets.length; i++) {
+            // 현재 목표 단어
+            int cnt = 101;
+            boolean flag = true;
+            char[] charArr = targets[i].toCharArray();
+            int sum = 0;
+            
+            // 목표단어의 문자 수
+            for (int j=0; j < charArr.length; j++) {
+                cnt = 101;
+                // 키맵의 개수
+                for (int k=0; k < keymap.length; k++) {
+                    char[] keyStr = keymap[k].toCharArray();                    
+                    
+                    // 하나의 키맵
+                    for (int t=0; t < keyStr.length; t++) {
+                        if (cnt < t+1) {
+                            break;
+                        }
+                        if (charArr[j] == keyStr[t]) {
+                            if (cnt < 101) {
+                                sum -= cnt;
+                            }
+                            sum += t+1;
+                            cnt = t+1;
+                            break;
+                        }
+                    }             
+                }
+                
+                if (cnt == 101) {
+                    flag = false;
+                    break;
+                }                
+            }
+            
+            if (flag == true) {
+                answer[i] = sum;
+            }
+            
         }
-        
         return answer;
     }
 }
