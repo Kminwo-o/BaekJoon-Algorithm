9c5b13b89943c47fb331a1ea9f1768902741ef5c
KwonOo <108923472+Kminwo-o@users.noreply.github.com>
2023-10-31 19:42:19 +0900

    [Silver II] Title: 주식, Time: 1016 ms, Memory: 324488 KB -BaekjoonHub

--- 백준/Silver/11501. 주식/주식.java
+++ 백준/Silver/11501. 주식/주식.java
@@ -3,20 +3,19 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.StringTokenizer;
 
-
 public class Main {
     static int T, N;
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         T = Integer.parseInt(br.readLine());
-        long[] answer = new long[T];
+        long[] ans = new long[T];
+
         for (int i = 0; i < T; i++) {
             N = Integer.parseInt(br.readLine());
-
             StringTokenizer st = new StringTokenizer(br.readLine());
             long[] arr = new long[N];
             for (int j = 0; j < N; j++) {
-                arr[j] = Integer.parseInt(st.nextToken());
+                arr[j] = Long.parseLong(st.nextToken());
             }
 
             long total = 0;
@@ -24,16 +23,17 @@ public class Main {
 
             for (int j = N-2; j >= 0; j--) {
                 if (now >= arr[j]) {
-                    total += now - arr[j];
+                    total += now -arr[j];
                 } else {
                     now = arr[j];
                 }
             }
 
-            answer[i] = total;
+            ans[i] = total;
         }
-        for (long ans : answer) {
-            System.out.println(ans);
+
+        for (long answer : ans) {
+            System.out.println(answer);
         }
     }
 }
