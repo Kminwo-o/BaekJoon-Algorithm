4fbf994078a9d40cf5ccfa8252ff1a3e6657d57d
KwonOo <108923472+Kminwo-o@users.noreply.github.com>
2023-11-21 10:01:36 +0900

    [level 1] Title: 숫자 문자열과 영단어, Time: 0.19 ms, Memory: 78.3 MB -BaekjoonHub

--- 프로그래머스/1/81301. 숫자 문자열과 영단어/숫자 문자열과 영단어.java
+++ 프로그래머스/1/81301. 숫자 문자열과 영단어/숫자 문자열과 영단어.java
@@ -1,11 +1,47 @@
+import java.util.*;
+
 class Solution {
     public int solution(String s) {
-        String[] strArr = {"zero", "one", "two", "three", "four", "five",
-                           "six", "seven", "eight", "nine"};
-        
-        for(int i = 0; i < 10; i++) {
-            s = s.replaceAll(strArr[i], Integer.toString(i));
+        int answer = 0;
+        StringBuilder sb = new StringBuilder();
+        char[] arr = s.toCharArray();
+        Map<String, Integer> map = new HashMap<>();
+        map.put("zero", 0);
+        map.put("one", 1);
+        map.put("two", 2);
+        map.put("three", 3);
+        map.put("four", 4);
+        map.put("five", 5);
+        map.put("six", 6);
+        map.put("seven", 7);
+        map.put("eight", 8);
+        map.put("nine", 9);
+
+        Stack<Integer> stack = new Stack<>();
+
+        int now = 1;
+        for (int i = 0; i < arr.length; i++) {
+            if (map.get(sb.toString()) != null) {
+                stack.push(map.get(sb.toString()));
+                sb.setLength(0);
+            }
+
+            if (arr[i] >= '0' && arr[i] <= '9') {
+                stack.push(Character.getNumericValue(arr[i]));
+                continue;
+            }
+            sb.append(arr[i]);
         }
-        return Integer.parseInt(s);
+
+        if (sb.length() != 0) {
+            stack.push(map.get(sb.toString()));
+            }
+
+        while (!stack.isEmpty()) {
+            answer += stack.pop() * now;
+            now *= 10;
+        }
+
+        return answer;
     }
 }
