6db2e95930d9301346f404e81df5e53b1d56f047
KwonOo <108923472+Kminwo-o@users.noreply.github.com>
2024-02-01 12:52:36 +0900

    Update 점수 계산.java

--- 백준/Silver/25710. 점수 계산/점수 계산.java
+++ 백준/Silver/25710. 점수 계산/점수 계산.java
@@ -9,6 +9,8 @@ class Main {
     static int n;
     static int[] arr;
     static int answer = 0;
+    
+    // 숫자 더하기 함수
     public static void plus (int num) {
         int ans = 0;
         while (num != 0) {
@@ -21,10 +23,13 @@ class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         n = Integer.parseInt(br.readLine());
+        // 중복 값이 존재할 경우 자기 자신과 더해주기 위해 중복 값 확인.
         int[] visited = new int[1000];
 
         Set<Integer> set = new HashSet<>();
         StringTokenizer st = new StringTokenizer(br.readLine());
+
+        // 중복 이후에는 굳이 여러번 찾아볼 필요가 없기 때문에 Set으로 중복 제거.
         for (int i = 0; i < n; i++) {
             int num = Integer.parseInt(st.nextToken());
             visited[num]++;
@@ -43,6 +48,7 @@ class Main {
         for (int i = 0; i < n; i++) {
             for (int j = i; j < n; j++) {
                 int num = arr[i] * arr[j];
+                // 중복이 아닌 경우 자기 자신이 아닐 경우에만, 중복인 경우 자기 자신과도 곱할 수 있어야 함.
                 if ((visited[arr[i]] < 2 && i != j) || (visited[arr[i]] > 1)) {
                     plus(num);
                 }
