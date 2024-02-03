b075866b0160bd0f2100ccea06f1c5feea7bbfd2
KwonOo <108923472+Kminwo-o@users.noreply.github.com>
2024-02-03 22:14:14 +0900

    [Gold IV] Title: 뮤탈리스크, Time: 84 ms, Memory: 14448 KB -BaekjoonHub

--- 백준/Gold/12869. 뮤탈리스크/뮤탈리스크.java
+++ 백준/Gold/12869. 뮤탈리스크/뮤탈리스크.java
@@ -6,20 +6,25 @@ import java.util.StringTokenizer;
 class Main {
     static int N;
     static int[][] attacks = {{-9, -3, -1}, {-9, -1, -3}, {-3, -9, -1}, {-3, -1, -9}, {-1, -9, -3}, {-1, -3, -9}};
+    // scv의 hp는 60이 최대
     static int[][][] dp = new int[61][61][61];
     static int[] scvList = new int[3];
     static int min = Integer.MAX_VALUE;
-
+    
+    
     public static void dfs(int[] scv, int cnt) {
         int s1 = scv[0];
         int s2 = scv[1];
         int s3 = scv[2];
-
+        
+        // 최소값 보다 많이 때렸으면 return
         if (min <= cnt) return;
+        // 현재 상태에 도달한 적이 있으면서 cnt가 이미 지금보다 많으면 return
         if (dp[s1][s2][s3] != 0 && dp[s1][s2][s3] <= cnt) return;
 
         dp[s1][s2][s3] = cnt;
 
+        // scv가 모두 사망하면 최소값 갱신
         if (s1 == 0 && s2 == 0 && s3 == 0) {
             min = Math.min(min, cnt);
             return;
@@ -38,7 +43,7 @@ class Main {
         for (int i = 0; i < N; i++) {
             scvList[i] = Integer.parseInt(st.nextToken());
         }
-
+        
         dfs(scvList, 0);
 
         System.out.println(min);
