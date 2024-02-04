20be9dd744ffe77e5f3b30a9601f17711044e894
KwonOo <108923472+Kminwo-o@users.noreply.github.com>
2024-02-04 17:20:23 +0900

    [level 3] Title: 양과 늑대, Time: 1.01 ms, Memory: 73.5 MB -BaekjoonHub

--- 프로그래머스/3/92343. 양과 늑대/양과 늑대.java
+++ 프로그래머스/3/92343. 양과 늑대/양과 늑대.java
@@ -6,29 +6,40 @@ class Solution {
     List<Integer>[] graph;
     int maxSheep = 0;
     public void dfs(int idx, int sheepCnt, int wolfCnt, List<Integer> nextList) {
+        // 현재 지점이 양이면 양 카운트 ++;
         if (treeInfo[idx] == 0) sheepCnt++;
+        // 아니면 늑대 ++;
         else wolfCnt++;
 
+        // 늑대랑 양이 같거나 더 많으면 리턴
         if (wolfCnt >= sheepCnt) return;
 
+        // 리턴 안됬으면 양 수 갱신
         maxSheep = Math.max(sheepCnt, maxSheep);
 
+        // 현재 지점에서 새로 체크 리스트 만들기.
         List<Integer> newList = new ArrayList<>();
         newList.addAll(nextList);
+
+        // 현재 idx는 빼기.
         newList.remove(Integer.valueOf(idx));
 
+        // 갈 수 있는 곳 추가.
         for (int next : graph[idx]) {
             newList.add(next);
         }
 
+        // 돌만큼 돌려주기.
         for (int next: newList) {
             dfs(next, sheepCnt, wolfCnt, newList);
         }
     }
     public int solution(int[] info, int[][] edges) {
+        // DFS에서 써야해서 편하게 전역으로
         treeInfo = info;
         graph = new List[info.length];
 
+        // 트리 만들어주기
         for (int i = 0; i < info.length; i++) {
             graph[i] = new ArrayList<>();
         }
