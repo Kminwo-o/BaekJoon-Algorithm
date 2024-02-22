75ba8b00413d1cf7e76556bc16a311a02fbef832
KwonOo <108923472+Kminwo-o@users.noreply.github.com>
2024-02-22 16:31:51 +0900

    [Gold II] Title: 줄 세우기, Time: 408 ms, Memory: 119664 KB -BaekjoonHub

--- 백준/Gold/7570. 줄 세우기/줄 세우기.java
+++ 백준/Gold/7570. 줄 세우기/줄 세우기.java
@@ -9,18 +9,14 @@ class Main {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         int n = Integer.parseInt(br.readLine());
 
-        int[] arr = new int[n];
+        int[] dp = new int[n+1];
+        int max = 0;
         StringTokenizer st = new StringTokenizer(br.readLine());
         for (int i = 0; i < n; i++) {
-            arr[i] = Integer.parseInt(st.nextToken());
+            int num = Integer.parseInt(st.nextToken());
+            dp[num] = dp[num-1] + 1;
+            max = Math.max(dp[num], max);
         }
-
-        int[] dp = new int[n+1];
-        for (int i = 0; i < n; i++) {
-            dp[arr[i]] = dp[arr[i]-1] + 1;
-        }
-
-        Arrays.sort(dp);
-        System.out.println(n - dp[n]);
+        System.out.println(n - max);
     }
 }
