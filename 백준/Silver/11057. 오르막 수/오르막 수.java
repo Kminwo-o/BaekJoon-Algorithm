1d5fd028829a39ef399a38b895431dfc4a315756
KwonOo <108923472+Kminwo-o@users.noreply.github.com>
2024-03-02 14:00:15 +0900

    [Silver I] Title: 오르막 수, Time: 84 ms, Memory: 11612 KB -BaekjoonHub

---
+++ 백준/Silver/11057. 오르막 수/오르막 수.java
@@ -0,0 +1,30 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int n = Integer.parseInt(br.readLine());
+
+        int[][] dp =  new int[n+1][10];
+        Arrays.fill(dp[1], 1);
+
+        for (int i = 2; i <= n; i++) {
+            for (int j = 0; j < 10; j++) {
+                for (int k = 0; k <= j; k++) {
+                    dp[i][j] += dp[i-1][k];
+                    dp[i][j] %= 10007;
+                }
+            }
+        }
+
+        int answer = 0;
+        for (int i = 0; i < 10; i++) {
+            answer += dp[n][i];
+        }
+
+        System.out.println(answer % 10007);
+    }
+}
