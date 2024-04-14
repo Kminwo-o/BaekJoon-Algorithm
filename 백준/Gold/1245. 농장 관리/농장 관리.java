d913c022a78db6692a3f12f046a89d8feb80d552
KwonOo <108923472+Kminwo-o@users.noreply.github.com>
2024-04-15 00:15:20 +0900

    [Gold V] Title: 농장 관리, Time: 116 ms, Memory: 13424 KB -BaekjoonHub

--- 백준/Gold/1245. 농장 관리/농장 관리.java
+++ 백준/Gold/1245. 농장 관리/농장 관리.java
@@ -7,15 +7,13 @@ public class Main {
     static int n, m, answer = 0;
     static int[][] mountainArr;
     static boolean[][] visited;
-    static boolean[][] mountainPeak;
     static int[][] dir = new int[][] {{-1, -1}, {-1, 0}, {-1, 1}, {0, 1}, {1, 1}, {1, 0}, {1, -1}, {0, -1}};
     static void bfs(int x, int y) {
-        visited = new boolean[n][m];
         Queue<int[]> queue = new LinkedList<>();
         queue.add(new int[] {x, y});
         visited[x][y] = true;
+        boolean peak = true;
 
-        List<int[]> peak = new ArrayList<>();
         while (!queue.isEmpty()) {
             int[] now = queue.poll();
 
@@ -24,24 +22,23 @@ public class Main {
                 int ny = now[1] + dir[i][1];
 
                 if (nx < 0 || nx >= n || ny < 0 || ny >= m) continue;
-                if (visited[nx][ny]) continue;
 
-                // 걸리는 경우 해당 봉우리는 봉우리가 아님
-                if (mountainArr[nx][ny] > mountainArr[x][y]) { return; }
-                if (mountainArr[x][y] == mountainArr[nx][ny]) {
-                    queue.add(new int[] {nx, ny});
-                    peak.add(new int[] {nx, ny});
+                if (mountainArr[x][y] < mountainArr[nx][ny]) {
+                    peak = false;
                 }
 
-                visited[nx][ny] = true;
+                if (visited[nx][ny]) continue;
+
+                if (mountainArr[x][y] == mountainArr[nx][ny]) {
+                    queue.add(new int[] {nx, ny});
+                    visited[nx][ny] = true;
+                }
             }
         }
 
-        for (int i = 0; i < peak.size(); i++) {
-            mountainPeak[peak.get(i)[0]][peak.get(i)[1]] = true;
+        if (peak) {
+            answer++;
         }
-
-        answer++;
     }
     public static void main(String[] args) throws IOException{
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
@@ -51,7 +48,7 @@ public class Main {
         m = Integer.parseInt(st.nextToken());
 
         mountainArr = new int[n][m];
-        mountainPeak = new boolean[n][m];
+        visited = new boolean[n][m];
 
         for (int i = 0; i < n; i++) {
             st = new StringTokenizer(br.readLine());
@@ -62,7 +59,7 @@ public class Main {
 
         for (int i = 0; i < n; i++) {
             for (int j = 0; j < m; j++) {
-                if (mountainArr[i][j] != 0 && !mountainPeak[i][j]) {
+                if (mountainArr[i][j] != 0 && !visited[i][j]) {
                     bfs(i, j);
                 }
             }
