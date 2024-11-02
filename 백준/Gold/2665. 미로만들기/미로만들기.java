141e8fd9c36684bfc16172f507686d849913772c
KwonOo <108923472+Kminwo-o@users.noreply.github.com>
2024-11-02 13:49:17 +0900

    [Gold IV] Title: 미로만들기, Time: 72 ms, Memory: 11932 KB -BaekjoonHub

--- 백준/Gold/2665. 미로만들기/미로만들기.java
+++ 백준/Gold/2665. 미로만들기/미로만들기.java
@@ -1,66 +1,74 @@
 import java.io.BufferedReader;
-import java.io.IOException;
 import java.io.InputStreamReader;
+import java.io.IOException;
 import java.util.*;
 
-class block {
-    int x;
-    int y;
-
-    block (int x, int y) {
-        this.x = x;
-        this.y = y;
-    }
-}
 public class Main {
-    static int[][] dir = {{-1, 0}, {1, 0}, {0, -1}, {0, 1}};
     static int n;
-    static String[] arr;
-    static int[][] visited;
+    static int[][] visited, map;
+    static int[][] dir = {{-1,0}, {1,0}, {0,-1}, {0,1}};
+    static PriorityQueue<Block> pq = new PriorityQueue();
 
-    public static void bfs(int x, int y) {
-        Queue<block> q = new LinkedList<>();
-        q.add(new block(x, y));
-        visited[x][y] = 0;
+    static class Block implements Comparable<Block>{
+        int x;
+        int y;
+        int change;
 
-        while (!q.isEmpty()) {
-            block now = q.poll();
+        public Block (int x, int y, int change) {
+            this.x = x;
+            this.y = y;
+            this.change = change;
+        }
+
+
+        @Override
+        public int compareTo(Block o) {
+            return this.change - o.change;
+        }
+    }
+    public static int dijkstra() {
+        visited[0][0] = 0;
+        pq.add(new Block(0, 0, 0));
+
+        while (!pq.isEmpty()) {
+            Block now = pq.poll();
+
+            if (visited[now.x][now.y] < now.change) continue;
 
             for (int i = 0; i < 4; i++) {
                 int nx = now.x + dir[i][0];
                 int ny = now.y + dir[i][1];
 
-                if ((nx >= 0 && nx < n) && (ny >= 0 && ny < n)) {
-                    if (visited[nx][ny] <= visited[now.x][now.y]) continue;
+                if (nx >= 0 && nx < n && ny >= 0 && ny < n) {
+                    int change = map[nx][ny] == 0 ? now.change + 1 : now.change;
 
-                    if (arr[nx].charAt(ny) == '1') {
-                        q.add(new block(nx, ny));
-                        visited[nx][ny] = visited[now.x][now.y];
-                    } else {
-                        q.add(new block(nx, ny));
-                        visited[nx][ny] = visited[now.x][now.y]+1;
+                    if (visited[nx][ny] > change) {
+                        visited[nx][ny] = change;
+                        pq.add(new Block(nx, ny, change));
                     }
                 }
             }
         }
+
+        return visited[n-1][n-1];
     }
 
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         n = Integer.parseInt(br.readLine());
-        arr = new String[n];
+
+        int INF = 2500;
+        map = new int[n][n];
         visited = new int[n][n];
 
         for (int i = 0; i < n; i++) {
-            Arrays.fill(visited[i], Integer.MAX_VALUE);
+            String line = br.readLine();
+            Arrays.fill(visited[i], INF);
+            for (int j = 0; j < n; j++) {
+                map[i][j] = Character.getNumericValue(line.charAt(j));
+            }
         }
 
-        for (int i = 0; i < n; i++) {
-            arr[i] = br.readLine();
-        }
-
-        bfs(0,0);
-
-        System.out.println(visited[n-1][n-1]);
+        System.out.println(dijkstra());
     }
 }
